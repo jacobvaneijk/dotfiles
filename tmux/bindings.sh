daa83c104cfb5c76f4a881effdf8282b4a74110e
Jacob van Eijk <jacob.vaneijk@gmail.com>
2017-02-05 21:10:03 +0100

    Update tmux bindings.

--- tmux/bindings.sh
+++ tmux/bindings.sh
@@ -10,23 +10,11 @@ bind r source-file ~/.config/tmux/.tmux.conf
 bind v split-window -h
 bind s split-window -v
 
-# Shift in combination with arrow keys to navigate between windows.
+# Prefix in combination with [hl] to navigate between windows.
 unbind n
 unbind p
-bind -n S-Left previous-window
-bind -n S-Right next-window
-
-# Move around panes with `hjkl`.
-bind -r h select-pane -L
-bind -r j select-pane -D
-bind -r k select-pane -U
-bind -r l select-pane -R
-
-# Resize panes with `HJKL`.
-bind -r H resize-pane -L 5
-bind -r J resize-pane -D 5
-bind -r K resize-pane -U 5
-bind -r L resize-pane -R 5
+bind -r h previous-window
+bind -r l next-window
 
 # Find a session and switch to it.
 bind C-f command-prompt -p find-session "switch-client -t %%"
