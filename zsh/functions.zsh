4ecdbe427b9dccd95d99c4c46536ecd1b8d17bad
Jacob van Eijk <jacob.vaneijk@gmail.com>
2018-08-06 11:46:07 +0200

    Add function to share files using transfer.sh.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -17,3 +17,41 @@ function up() {
 function fif() {
     grep -rnw '.' -e "$@"
 }
+
+# Transfer a file using transfer.sh.
+transfer() {
+    if [ $# -eq 0 ];
+    then
+        echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"
+        return 1
+    fi
+
+    tmpfile=$( mktemp -t transferXXX )
+    file=$1
+
+    if tty -s;
+    then
+        basefile=$(basename "$file" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+
+        if [ ! -e $file ];
+        then
+            echo "File $file doesn't exists."
+            return 1
+        fi
+
+        if [ -d $file ];
+        then
+            zipfile=$( mktemp -t transferXXX.zip )
+            cd $(dirname $file) && zip -r -q - $(basename $file) >> $zipfile
+            curl --progress-bar --upload-file "$zipfile" "https://transfer.sh/$basefile.zip" >> $tmpfile
+            rm -f $zipfile
+        else
+            curl --progress-bar --upload-file "$file" "https://transfer.sh/$basefile" >> $tmpfile
+        fi
+    else
+        curl --progress-bar --upload-file "-" "https://transfer.sh/$file" >> $tmpfile
+    fi
+
+    cat $tmpfile
+    rm -f $tmpfile
+}
